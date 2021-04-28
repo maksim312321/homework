78219202d2c3e68bc5c37540ab01d61fcbac564c
Maksim Zelenin <maksim.zelenin.01@bk.ru>
2021-04-28 23:02:24 +0500

    week3 | api требует доработки

--- офисная_мебель_верстка/src/js/app.js
+++ офисная_мебель_верстка/src/js/app.js
@@ -1,6 +1,8 @@
+var geo;
 ymaps.ready(function (){
-    var city = ymaps.geolocation.city;
+    geo = ymaps.geolocation.city;
     city = document.getElementById('city');
+    city.innerHTML = geo;
     var tel = document.getElementById('tel');
     if(city.text != "Москва"){
     tel.innerHTML= "+7 900 0000 777"
