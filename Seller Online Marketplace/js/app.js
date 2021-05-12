3ba60d8132bed9930a3229b5e9491a96722e8564
Maksim Zelenin <maksim.zelenin.01@bk.ru>
2021-05-12 18:22:02 +0500

    week5 | черновая главная страница

--- Seller Online Marketplace/js/app.js
+++ Seller Online Marketplace/js/app.js
@@ -1,35 +1,66 @@
 $(document).ready(function(){
     let position = 0;
-    const slidesToShow = 1;
-    const slidesToScroll = 1;
-    const container = $('.blog__slider');
-    const track = $('.slider__track');
-    const item = $('.slider__item');
-    const btnPrev = $('.slider__prev');
-    const btnNext = $('.slider__next');
-    itemsCount = item.lenght;
-    const itemWidth = 742;
+    const track = $('.slider__track');//все слайды
+    const item = $('.slider__item');//слайд
+    const btnPrev = $('.slider__prev');//кнопка
+    const btnNext = $('.slider__next');//кнопка
+    itemsCount = item.size();//$('.slider__img').lenght;
+    let itemWidth = 742;//ширину задал фикс, не успеваю найти адекватного способа нахождения ширины картинки
+    let hasClick = false;
 
-    btnNext.click(function(){
-        position -= itemWidth
+
+    function auto(){
+        if(hasClick == false){
+        position -= itemWidth;  
+        checkBtns();
+        //перемещение на след слайд    
         track.css({
             transform: `translateX(${position}px)`,
         })
-        heckBtns();
+    }
+    }
+    let timerId = setInterval(auto,2000);
+    setTimeout(() => {clearInterval(timerId)}, 2000*(itemsCount-1));
+
+    //ф-я отключения кнопок по условию
+    const checkBtns = () => {
+        btnPrev.prop('disabled', position === 0);
+        btnNext.prop('disabled', position <= -(itemsCount - 1)*itemWidth);
+    };
+
+    btnNext.click(function(){
+        hasClick =true;
+        position -= itemWidth;  
+        checkBtns();
+        //перемещение на след слайд    
+        track.css({
+            transform: `translateX(${position}px)`,
+        })
+        
     });
 
     btnPrev.click(function(){
+        hasClick =true;
         position += itemWidth
+        checkBtns();
+        //перемещение на след слайд        
         track.css({
             transform: `translateX(${position}px)`,
         })
-        checkBtns();
+        
     });
+    checkBtns();
+});
 
-    const checkBtns = () => {
-        btnPrev.prop('disabled', position == 0);
-        btnPrev.prop('disabled', position <= -(itemsCount - slidesToShow)*itemWidth
-        );
-    };
-
+new Swiper('.viewed', {
+    //стрелки
+    navigation: {
+        nextEl: '.viewed__next',
+    },
+});
+new Swiper('.reviews', {
+    //стрелки
+    navigation: {
+        nextEl: '.reviews__next',
+    },
 });
